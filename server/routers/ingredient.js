0cd94b2e519e0ce73366d493839277913d9bb147
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2024-11-23 12:32:02 +0100

    first API doc

--- server/routers/ingredient.js
+++ server/routers/ingredient.js
@@ -32,7 +32,26 @@ const conn =  mariadb.createPool({
     dateStrings: true
   });
 
-
+/**
+ * @api [get] /ingredient/name
+ * tags : 
+ *  - Ingredients
+ * description: "Return the ingredient informations for the given name"
+ * parameters:
+ * - name: name
+ *   in: query
+ *   description: Name of the ingredient to get
+ *   required: true
+ *   type: string
+ *
+ * responses:
+ *   "200":
+ *     description: "Ingredient infos"
+ *   "204":
+ *      description: "There isn't an ingredient named like requested"
+ *   "405":
+ *      description: "Something is wrong in request parameters"
+ */
 router.get("/name", async (req, res) => {
     if (req.query.name === undefined || !(typeof req.query.name === 'string')){
         res.status(405).send("undefined ingredient name");
@@ -50,6 +69,26 @@ router.get("/name", async (req, res) => {
     }
 });
 
+/**
+ * @api [get] /ingredient/units
+ * tags : 
+ *  - Ingredients
+ * description: "Return the units linked to the given ingredient name"
+ * parameters:
+ * - name: name
+ *   in: query
+ *   description: Name of the ingredient
+ *   required: true
+ *   type: string
+ *
+ * responses:
+ *   "200":
+ *     description: "Ingredient units"
+ *   "204":
+ *      description: "There isn't units linked to the ingredient name specified"
+ *   "405":
+ *      description: "Something is wrong in request parameters"
+ */
 router.get("/units", async (req, res) => {
     if (req.query.name === undefined || !(typeof req.query.name === 'string')){
         res.status(405).send("undefined ingredient name");
@@ -67,7 +106,28 @@ router.get("/units", async (req, res) => {
     }
 });
 
-
+/**
+ * @api [post] /ingredient/add
+ * tags : 
+ *  - Ingredients
+ * description: "Add the specified ingredient to the known ingredients"
+ * parameters:
+ * - name: name
+ *   in: query
+ *   description: Name of the ingredient
+ *   required: true
+ *   type: string
+ * - name: type
+ *   in: query
+ *   description: Type of the ingredient
+ *   required: true
+ *   type: string
+ * responses:
+ *   "200":
+ *     description: "Ingredient has been added successfuly or already exixsts"
+ *   "405":
+ *      description: "Something is wrong in request parameters"
+ */
 router.post("/add", async (req, res) => {
     if (req.body.name === undefined || !(typeof req.body.name === 'string') || req.body.type === undefined || !(typeof req.body.type === 'string')){
         res.status(405).send("undefined ingredient name or type");
