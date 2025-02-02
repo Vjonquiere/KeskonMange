0cd8acb0f9bebcd715559b2dff5c5666fb8a4e8f
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-02-02 18:39:24 +0100

    setup 1 test for CI

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -10,4 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:client/main.dart';
 
-void main() {}
+void main() {
+  group('Empty tests', () {
+    test('First empty', () {
+      expect(0, 0);
+    });
+  });
+}
