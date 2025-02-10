46c67e83bffed244e283b5b923b48eb12709060b
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-02-10 18:51:17 +0100

    apply server style + check in CI

--- server/module/constants.js
+++ server/module/constants.js
@@ -1,39 +1,37 @@
-
 const units = {
-    "liquid":["l", "g"], 
-    "fruit":["piece", "g"],
-    "vegetable":["piece", "g"],
-    "oil":["g", "PM"], 
-    "grocerie":["g", "PM"], 
-    "fish":["g", "piece"], 
-    "meat":["g", "piece"],
-    "cremerie":["g", "l"],
-    "egg":["piece"],
-    "herb":["bunch", "g"]
-}
+  liquid: ["l", "g"],
+  fruit: ["piece", "g"],
+  vegetable: ["piece", "g"],
+  oil: ["g", "PM"],
+  grocerie: ["g", "PM"],
+  fish: ["g", "piece"],
+  meat: ["g", "piece"],
+  cremerie: ["g", "l"],
+  egg: ["piece"],
+  herb: ["bunch", "g"],
+};
 
 const ALLERGEN_NUMBER = 14;
 
 const allergens = {
-    "Gluten": 1,
-    "Fish": 2,
-    "Peanuts":3,
-    "Celery":4,
-    "Sulfates":5,
-    "Lupin":6,
-    "Eggs":7,
-    "Sesame":8,
-    "Mustard":9,
-    "Mollusks":10,
-    "Crustaceans":11,
-    "Soy":12,
-    "Nuts":13,
-    "Milk":14
-}
-
+  Gluten: 1,
+  Fish: 2,
+  Peanuts: 3,
+  Celery: 4,
+  Sulfates: 5,
+  Lupin: 6,
+  Eggs: 7,
+  Sesame: 8,
+  Mustard: 9,
+  Mollusks: 10,
+  Crustaceans: 11,
+  Soy: 12,
+  Nuts: 13,
+  Milk: 14,
+};
 
 module.exports = {
-    units : units,
-    allergens : allergens,
-    ALLERGEN_NUMBER : ALLERGEN_NUMBER
+  units: units,
+  allergens: allergens,
+  ALLERGEN_NUMBER: ALLERGEN_NUMBER,
 };
--- server/routers/recipeBook.js
+++ server/routers/recipeBook.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const database = require('../module/database');
+const database = require("../module/database");
 const conn = database.conn;
-const needAuth = require('../module/token').checkApiKey;
-var bodyParser = require('body-parser');
+const needAuth = require("../module/token").checkApiKey;
+var bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
 router.use(
@@ -12,21 +12,37 @@ router.use(
   }),
 );
 
-
-async function bookExist(bookId){
-  let exists = await conn.query(`SELECT COUNT(id) FROM recipe_books WHERE id=?;`, [bookId]);
-  return exists[0]['COUNT(id)'] <= 0;
+async function bookExist(bookId) {
+  let exists = await conn.query(
+    `SELECT COUNT(id) FROM recipe_books WHERE id=?;`,
+    [bookId],
+  );
+  return exists[0]["COUNT(id)"] <= 0;
 }
 
-async function userHasReadAccess(bookId, userId){
-  const isPublic = await conn.query(`SELECT id FROM recipe_books WHERE id = ? AND visibility = 1`, [bookId]);
-  const isOwner = await conn.query(`SELECT id FROM recipe_books WHERE id = ? AND owner = ? `, [bookId, userId]);
-  const isAuthorized = await conn.query(`SELECT id FROM recipe_books JOIN recipe_book_access ON recipe_books.id = recipe_book_access.bookId WHERE recipe_book_access.bookId = ? AND recipe_book_access.userId = ?;`, [bookId, userId]);
-  return isPublic.length >= 1 || isOwner.length >= 1 || isAuthorized.length >= 1;
+async function userHasReadAccess(bookId, userId) {
+  const isPublic = await conn.query(
+    `SELECT id FROM recipe_books WHERE id = ? AND visibility = 1`,
+    [bookId],
+  );
+  const isOwner = await conn.query(
+    `SELECT id FROM recipe_books WHERE id = ? AND owner = ? `,
+    [bookId, userId],
+  );
+  const isAuthorized = await conn.query(
+    `SELECT id FROM recipe_books JOIN recipe_book_access ON recipe_books.id = recipe_book_access.bookId WHERE recipe_book_access.bookId = ? AND recipe_book_access.userId = ?;`,
+    [bookId, userId],
+  );
+  return (
+    isPublic.length >= 1 || isOwner.length >= 1 || isAuthorized.length >= 1
+  );
 }
 
 async function isOwner(bookId, userId) {
-  const book = await conn.query(`SELECT id FROM recipe_books WHERE id = ? AND owner = ?`, [bookId, userId]);
+  const book = await conn.query(
+    `SELECT id FROM recipe_books WHERE id = ? AND owner = ?`,
+    [bookId, userId],
+  );
   return book.length == 1;
 }
 
@@ -48,28 +64,29 @@ async function isOwner(bookId, userId) {
  *      description: "Name parameter is wrong"
  */
 router.post("/create", needAuth, async (req, res) => {
-    if (req.query.name === undefined){
-        res.status(400).send("Need to specify a book name");
-        return;
-    }
+  if (req.query.name === undefined) {
+    res.status(400).send("Need to specify a book name");
+    return;
+  }
 
-    const reg = /^[a-zA-Z0-9\s]+$/; // only chars, spaces and numbers 
-    if (reg.test(req.query.name) && req.query.name.length <= 32){
-        try {
-            await conn.query(`INSERT INTO recipe_books VALUES (null, ?, ?, 1);`, [req.query.name, req.user.userId]);
-        } catch (error) {
-            console.log(error)
-            res.sendStatus(500);
-            return;
-        }
-        
-    } else {
-      res.status(400).send("Book name is invalid");
+  const reg = /^[a-zA-Z0-9\s]+$/; // only chars, spaces and numbers
+  if (reg.test(req.query.name) && req.query.name.length <= 32) {
+    try {
+      await conn.query(`INSERT INTO recipe_books VALUES (null, ?, ?, 1);`, [
+        req.query.name,
+        req.user.userId,
+      ]);
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
       return;
     }
-    res.sendStatus(200);
-
-})
+  } else {
+    res.status(400).send("Book name is invalid");
+    return;
+  }
+  res.sendStatus(200);
+});
 
 /**
  * @api [delete] /books/delete
@@ -91,21 +108,25 @@ router.post("/create", needAuth, async (req, res) => {
  *     description: "Not allowed"
  */
 router.delete("/delete", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined){
+  if (req.query.bookId === undefined) {
     res.status(400).send("You need to specify a bookId to delete");
     return;
   }
   try {
-    if ((await bookExist(req.query.bookId))) return res.sendStatus(200);
-    if (!(await isOwner(req.query.bookId, req.user.userId))) return res.sendStatus(403);
-    await conn.query(`DELETE FROM recipe_books WHERE id=? AND owner = ?;`, [req.query.bookId, req.user.userId]);
+    if (await bookExist(req.query.bookId)) return res.sendStatus(200);
+    if (!(await isOwner(req.query.bookId, req.user.userId)))
+      return res.sendStatus(403);
+    await conn.query(`DELETE FROM recipe_books WHERE id=? AND owner = ?;`, [
+      req.query.bookId,
+      req.user.userId,
+    ]);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.sendStatus(500);
     return;
   }
   res.sendStatus(200);
-})
+});
 
 /**
  * @api [post] /books/share
@@ -134,25 +155,31 @@ router.delete("/delete", needAuth, async (req, res) => {
  *      description: "Not allowed"
  */
 router.post("/share", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined || req.query.userId === undefined){
-    res.status(400).send("Specify a bookId and the user you want to share the book with");
+  if (req.query.bookId === undefined || req.query.userId === undefined) {
+    res
+      .status(400)
+      .send("Specify a bookId and the user you want to share the book with");
     return;
   }
   try {
-    if (await bookExist(req.query.bookId)){
+    if (await bookExist(req.query.bookId)) {
       res.status(500).send("Can't share given book: no matching id");
       return;
-    }    
-    if (! (await isOwner(req.query.bookId, req.user.userId))) return res.sendStatus(403); // Status 500 if requester is not the owner
+    }
+    if (!(await isOwner(req.query.bookId, req.user.userId)))
+      return res.sendStatus(403); // Status 500 if requester is not the owner
     // TODO: Check for duplicates
-    await conn.query(`INSERT INTO recipe_book_access VALUES (?,?);`, [req.query.bookId, req.query.userId]);
+    await conn.query(`INSERT INTO recipe_book_access VALUES (?,?);`, [
+      req.query.bookId,
+      req.query.userId,
+    ]);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.sendStatus(500);
     return;
   }
   res.sendStatus(200);
-})
+});
 
 /**
  * @api [get] /books/recipes
@@ -176,32 +203,35 @@ router.post("/share", needAuth, async (req, res) => {
  *      description: "The book does not exists"
  */
 router.get("/recipes", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined){
-      res.status(400).send("Need to specify a bookId");
-      return;
+  if (req.query.bookId === undefined) {
+    res.status(400).send("Need to specify a bookId");
+    return;
   }
   try {
-      if (await bookExist(req.query.bookId)){
-        res.status(500).send("Can't get recipes from given book: no matching id");
-        return;
-      }
-      if (!(await userHasReadAccess(req.query.bookId, req.user.userId))) return res.sendStatus(403);
-      const recipesRaw = await conn.query(`SELECT recipeId FROM recipe_book_links WHERE bookId = ?;`, [req.query.bookId]);
-      let recipes = Array.from(recipesRaw);
-      let ids = [];
-      for(const recipe  of recipes){
-        ids.push(recipe["recipeId"]);
-      }
-      res.send(JSON.stringify({"recipes": ids}));
+    if (await bookExist(req.query.bookId)) {
+      res.status(500).send("Can't get recipes from given book: no matching id");
       return;
+    }
+    if (!(await userHasReadAccess(req.query.bookId, req.user.userId)))
+      return res.sendStatus(403);
+    const recipesRaw = await conn.query(
+      `SELECT recipeId FROM recipe_book_links WHERE bookId = ?;`,
+      [req.query.bookId],
+    );
+    let recipes = Array.from(recipesRaw);
+    let ids = [];
+    for (const recipe of recipes) {
+      ids.push(recipe["recipeId"]);
+    }
+    res.send(JSON.stringify({ recipes: ids }));
+    return;
   } catch (error) {
-      console.log(error)
-      res.sendStatus(500);
-      return;
+    console.log(error);
+    res.sendStatus(500);
+    return;
   }
   res.sendStatus(200);
-
-})
+});
 
 /**
  * @api [get] /books/general_information
@@ -223,31 +253,43 @@ router.get("/recipes", needAuth, async (req, res) => {
  *      description: "BookId parameter is wrong"
  */
 router.get("/general_information", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined){
+  if (req.query.bookId === undefined) {
     res.status(400).send("You need to specify a bookId to search for");
     return;
   }
   try {
-    const info = await conn.query(`SELECT * FROM recipe_books WHERE id=?;`, [req.query.bookId]);
+    const info = await conn.query(`SELECT * FROM recipe_books WHERE id=?;`, [
+      req.query.bookId,
+    ]);
     let bookInfo = Array.from(info); // Casting to Array to check length
-        if (bookInfo.length <= 0){
-            res.sendStatus(204); // No book found with the given id
-            return;
-        } else if (! (await userHasReadAccess(req.query.bookId, req.user.userId))){
-          return res.sendStatus(403);
-        } else {
-            let countRes =  await conn.query(`SELECT COUNT(bookId) FROM recipe_book_links WHERE bookId=?;`, [req.query.bookId]);
-            let count = Number(countRes[0]['COUNT(bookId)']);
-            res.send(JSON.stringify({"id": bookInfo[0]["id"], "name":  bookInfo[0]["name"], "owner": bookInfo[0]["owner"], "visibility": bookInfo[0]["visibility"], "recipe_count": count }));
-            return;
-        }
+    if (bookInfo.length <= 0) {
+      res.sendStatus(204); // No book found with the given id
+      return;
+    } else if (!(await userHasReadAccess(req.query.bookId, req.user.userId))) {
+      return res.sendStatus(403);
+    } else {
+      let countRes = await conn.query(
+        `SELECT COUNT(bookId) FROM recipe_book_links WHERE bookId=?;`,
+        [req.query.bookId],
+      );
+      let count = Number(countRes[0]["COUNT(bookId)"]);
+      res.send(
+        JSON.stringify({
+          id: bookInfo[0]["id"],
+          name: bookInfo[0]["name"],
+          owner: bookInfo[0]["owner"],
+          visibility: bookInfo[0]["visibility"],
+          recipe_count: count,
+        }),
+      );
+      return;
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.sendStatus(500);
     return;
   }
-
-})
+});
 
 /**
  * @api [get] /books/id
@@ -268,29 +310,31 @@ router.get("/general_information", needAuth, async (req, res) => {
  *   "400":
  *      description: "Name parameter is wrong"
  */
-router.get("/id", async (req, res) => { //TODO: check for multiple names
-  if (req.query.bookName === undefined){
+router.get("/id", async (req, res) => {
+  //TODO: check for multiple names
+  if (req.query.bookName === undefined) {
     res.status(400).send("You need to specify a book name to search");
     return;
   }
   try {
-    const info = await conn.query(`SELECT * FROM recipe_books WHERE name=?;`, [req.query.bookName]);
+    const info = await conn.query(`SELECT * FROM recipe_books WHERE name=?;`, [
+      req.query.bookName,
+    ]);
     let bookInfo = Array.from(info); // Casting to Array to check length
-        if (bookInfo.length <= 0){
-            res.sendStatus(204); // No book found with the given id
-            return;
-        // check the owner name
-        } else {
-            res.send(JSON.stringify({"id": bookInfo[0]["id"]}));
-            return;
-        }
+    if (bookInfo.length <= 0) {
+      res.sendStatus(204); // No book found with the given id
+      return;
+      // check the owner name
+    } else {
+      res.send(JSON.stringify({ id: bookInfo[0]["id"] }));
+      return;
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.sendStatus(500);
     return;
   }
-
-})
+});
 
 /**
  * @api [post] /books/recipe/add
@@ -302,7 +346,7 @@ router.get("/id", async (req, res) => { //TODO: check for multiple names
  *   description: Id of the book
  *   required: true
  *   type: integer
-  * - name: recipeId
+ * - name: recipeId
  *   in: query
  *   description: Id of the recipe
  *   required: true
@@ -319,26 +363,31 @@ router.get("/id", async (req, res) => { //TODO: check for multiple names
  *     description: "Not allowed"
  */
 router.post("/recipe/add", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined || req.query.recipeId === undefined){
+  if (req.query.bookId === undefined || req.query.recipeId === undefined) {
     res.status(400).send("You need to specify a bookId and a recipeId");
     return;
   }
   let date = new Date();
-  let formattedDate = date.toISOString().split('T')[0];
+  let formattedDate = date.toISOString().split("T")[0];
   try {
-    if (await bookExist(req.query.bookId)){
+    if (await bookExist(req.query.bookId)) {
       res.status(500).send("Can't add recipe to given book: no matching id");
       return;
     }
-    if (!(await isOwner(req.query.bookId, req.user.userId))) return res.sendStatus(403);
-    await conn.query(`INSERT INTO recipe_book_links VALUES (?, ?, ?);`, [req.query.bookId, req.query.recipeId, formattedDate]);
+    if (!(await isOwner(req.query.bookId, req.user.userId)))
+      return res.sendStatus(403);
+    await conn.query(`INSERT INTO recipe_book_links VALUES (?, ?, ?);`, [
+      req.query.bookId,
+      req.query.recipeId,
+      formattedDate,
+    ]);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
     return;
   }
   res.sendStatus(200);
-})
+});
 
 /**
  * @api [delete] /books/share
@@ -350,7 +399,7 @@ router.post("/recipe/add", needAuth, async (req, res) => {
  *   description: Id of the book
  *   required: true
  *   type: integer
-* - name: userId
+ * - name: userId
  *   in: query
  *   description: Id of the user
  *   required: true
@@ -365,24 +414,27 @@ router.post("/recipe/add", needAuth, async (req, res) => {
  *      description: "Not allowed"
  */
 router.delete("/share", needAuth, async (req, res) => {
-  if (req.query.bookId === undefined || req.query.userId === undefined){
+  if (req.query.bookId === undefined || req.query.userId === undefined) {
     res.status(400).send("You need to specify a bookId and a userId");
     return;
   }
   try {
-    if (!(await isOwner(req.query.bookId, req.user.userId))) return res.sendStatus(403);
-    await conn.query(`DELETE FROM recipe_book_access WHERE bookId = ? AND userId = ?;`, [req.query.bookId, req.query.userId]);
+    if (!(await isOwner(req.query.bookId, req.user.userId)))
+      return res.sendStatus(403);
+    await conn.query(
+      `DELETE FROM recipe_book_access WHERE bookId = ? AND userId = ?;`,
+      [req.query.bookId, req.query.userId],
+    );
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
     return;
   }
   res.sendStatus(200);
-})
+});
 
 router.closeServer = () => {
-    console.log("Calendar Closed");
+  console.log("Calendar Closed");
 };
-
 
 module.exports = router;
