439d6a73f0a48e89e0fa1621f119c2df93650dda
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-07-12 12:01:08 +0200

    ingredient units + load sample recipes/ingredients for mock repos + variables file

--- client/lib/model/ingredient.dart
+++ client/lib/model/ingredient.dart
@@ -1,14 +1,17 @@
+import 'package:client/model/ingredient_units.dart';
+
 class Ingredient {
   final String name;
+  final Units type;
 
-  Ingredient(this.name);
+  Ingredient(this.name, this.type);
 
   factory Ingredient.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
         'name': String name,
       } =>
-        Ingredient(name),
+        Ingredient(name, Units.SpecialUnits),
       _ => throw const FormatException('Failed to load ingredient.'),
     };
   }
---
+++ client/lib/model/ingredient_units.dart
@@ -0,0 +1,46 @@
+enum WholeItemsUnits {
+  Piece,
+  Clove,
+  Slice,
+  Stick,
+  Head,
+  Leaf,
+  Can,
+  Bottle,
+  Jar,
+  Package,
+  Box,
+  Bunch,
+}
+
+enum VolumeUnits {
+  Teaspoon,
+  Tablespoon,
+  FluidOunce,
+  Cup,
+  Pint,
+  Quart,
+  Gallon,
+  Millimiter,
+  Liter
+}
+
+enum WeightUnits {
+  Gram,
+  Kilogram,
+  Ounce,
+  Pound,
+}
+
+enum SpecialUnits {
+  StickOfButter,
+  EggSizes,
+  Sheet,
+}
+
+enum Units {
+  WholeItemsUnits,
+  VolumeUnits,
+  WeightUnits,
+  SpecialUnits,
+}
