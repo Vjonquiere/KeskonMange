ec72af266831adb942782da19df18f5db2db6eba
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-08-24 23:51:39 +0200

    allergerns selector widget factorisation + ingredient creation view pattern

---
+++ client/lib/features/ingredient_creation/viewmodels/ingredient_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:client/core/ViewModel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+
+class IngredientCreationViewModel extends ViewModel {
+  IngredientCreationViewModel() {}
+}
