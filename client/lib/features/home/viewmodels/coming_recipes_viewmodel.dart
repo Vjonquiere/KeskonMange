692a7de2519778e98b35dd2001a5969a40625a8a
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-09-24 14:48:38 +0200

    Recipe preview update (show calendar infos + reduce infos number)

--- client/lib/features/home/viewmodels/coming_recipes_viewmodel.dart
+++ client/lib/features/home/viewmodels/coming_recipes_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:client/model/recipe/preview.dart';
 
 import '../../../core/view_model.dart';
@@ -7,8 +9,10 @@ import '../../../data/usecases/get_recipe_from_id_use_case.dart';
 
 class ComingRecipesViewModel extends ViewModel {
   List<RecipePreview> _recipes = <RecipePreview>[];
+  Map<RecipePreview, DateTime?> _calendarEntries = {};
 
   List<RecipePreview> get recipes => _recipes;
+  Map<RecipePreview, DateTime?> get calendarEntries => _calendarEntries;
 
   ComingRecipesViewModel() {
     getRecipes();
@@ -18,6 +22,17 @@ class ComingRecipesViewModel extends ViewModel {
     _recipes = await RepositoriesManager()
         .getCalendarRepository()
         .getNextPlannedRecipes(3);
+    for (RecipePreview recipe in _recipes) {
+      List<DateTime> entries = await RepositoriesManager()
+          .getCalendarRepository()
+          .getDateFromPlannedRecipe(recipe.id);
+      if (entries.isEmpty) {
+        _calendarEntries[recipe] = null;
+      } else {
+        _calendarEntries[recipe] = entries.first;
+      }
+    }
+
     /*final List<int> ids = <int>[1, 2, 3];
     final List<RecipePreview> recipes = <RecipePreview>[];
     for (int id = 0; id < ids.length; id++) {
