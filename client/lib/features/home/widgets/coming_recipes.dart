f6d53ae346225f417dfe9e62c8ea29ca7cdd19e5
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-09-25 11:04:48 +0200

    home screen no planned recipes widget

--- client/lib/features/home/widgets/coming_recipes.dart
+++ client/lib/features/home/widgets/coming_recipes.dart
@@ -1,11 +1,15 @@
+import 'package:client/core/widgets/custom_buttons.dart';
 import 'package:client/core/widgets/custom_dividers.dart';
 import 'package:client/features/home/widgets/recipe_preview.dart';
+import 'package:client/features/recipe_search/viewmodels/search_page_viewmodel.dart';
+import 'package:client/features/recipe_search/views/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/widget_states.dart';
 import '../../../l10n/app_localizations.dart';
 import '../../../model/recipe/preview.dart' as rp_model;
+import '../../../utils/app_colors.dart';
 import '../viewmodels/coming_recipes_viewmodel.dart';
 
 class ComingRecipes extends StatelessWidget {
@@ -26,6 +30,27 @@ class ComingRecipes extends StatelessWidget {
     );
   }
 
+  Widget _emptyPlannedRecipes(BuildContext context) {
+    return Column(
+      children: [
+        const Text("You don't have any schedule recipe!"),
+        CustomButton(
+          text: "Find recipes",
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute<SearchPage>(
+              builder: (BuildContext context) =>
+                  ChangeNotifierProvider<SearchPageViewModel>(
+                create: (BuildContext context) => SearchPageViewModel(),
+                child: const SearchPage(),
+              ),
+            ),
+          ),
+          color: AppColors.blue,
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final ComingRecipesViewModel viewModel =
@@ -34,12 +59,14 @@ class ComingRecipes extends StatelessWidget {
       WidgetStates.idle => Container(),
       WidgetStates.loading => const CircularProgressIndicator(),
       WidgetStates.error => Text(AppLocalizations.of(context)!.error),
-      WidgetStates.ready => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(
-              viewModel.recipes.length,
-              (int index) => _homeRecipePreview(
-                  viewModel.recipes.elementAt(index), viewModel))),
+      WidgetStates.ready => viewModel.recipes.isEmpty
+          ? _emptyPlannedRecipes(context)
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  viewModel.recipes.length,
+                  (int index) => _homeRecipePreview(
+                      viewModel.recipes.elementAt(index), viewModel))),
       WidgetStates.dispose => const Text("dispose"),
     };
   }
